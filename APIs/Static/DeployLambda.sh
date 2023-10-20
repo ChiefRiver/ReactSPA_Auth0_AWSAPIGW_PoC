77c912b6113592bb224ea98f36f4f4a0da86ed1b
Grant Sims <sims.grant@gmail.com>
2023-10-20 13:06:48 -0400

    removed deployment function

--- APIs/Static/DeployLambda.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd Function && zip -r static.zip * && aws lambda update-function-code --function-name kickitsoon-dev-Lambda-static --zip-file fileb://static.zip --region us-east-1 && rm static.zip
-
